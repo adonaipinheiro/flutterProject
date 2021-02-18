f434de2efad535de9ed008afc207407636005e00
Adonai Pinheiro <adonaijpinheiro@gmail.com>
2021-02-18 13:00:58 -0300

    Changing page

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,40 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MeuApp());
+}
+
+class MeuApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Center(
+              child: Text('Meu App'),
+            ),
+          ),
+          body: Column(
+            children: [
+              Expanded(child: Container(color: Colors.red)),
+              Row(
+                children: [
+                  Expanded(child: Container(height: 100, color: Colors.blue)),
+                  Expanded(child: Container(height: 100, color: Colors.green)),
+                ],
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              ),
+              Row(
+                children: [
+                  Expanded(child: Container(height: 100, color: Colors.yellow)),
+                  Expanded(child: Container(height: 100, color: Colors.orange)),
+                  Expanded(
+                      child: Container(height: 100, color: Colors.orange[100])),
+                ],
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              ),
+            ],
+          )),
+    );
+  }
+}
